c27a7abe97fd268c77f513e2ac72480ba4b67627
Barak Kaldess <ibarakkaldess@gmail.com>
2022-12-08 17:14:09 +0200

    Add Header Component

--- src/cmps/header.jsx
+++ src/cmps/header.jsx
@@ -1,12 +1,27 @@
-import { Link, NavLink } from "react-router-dom"
+import { useEffect } from "react";
+import { Link, NavLink, useLocation, useNavigate, } from "react-router-dom"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export const Header = () => {
 
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (location.pathname === '/') navigate('/home')
+    }, [])
+
     return (
         <div className="header">
-            Header cmp
+            <nav className="main-nav">
+                <Link to="/home" className="item logo">Weather App</Link>
+                <div>
+                    <NavLink to="/home" className="item">Home</NavLink>
+                    <NavLink to="/favorites" className="item">Favorites</NavLink>
+                </div>
+            </nav>
+            <ToastContainer />
         </div>
     )
 }
